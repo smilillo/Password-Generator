62e4f7f89525e5ff8e95f9d5777396cce381056e
smilillo <sofiemilillo@gmail.com>
2023-02-10 13:28:03 -0500

    added isNaN option for password length error

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 
 // Variables Needed for Password Criteria
-// let passwordLength = ;
 let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -22,16 +21,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // prompts for password criteria
-  // 
+
   // prompt for length between 8 and 128 characters
   let passwordLength = parseInt(prompt("Please enter the desired length of your password as an integer between 8 and 128."));
-    if (passwordLength < 8 || passwordLength > 128) {
+  // alerts user to try again if length entered does not meet parameters
+    if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
       window.alert("The password length must be an integer between 8 and 128. Please try again.");
     }
 
 
-  //  passwordLength = 
-  //   if (passwordLegth < 8 || > 128) return: "Please select a length between 8 and 128 characters"
   // prompt for whether or not to include lowercase, uppercase, numeric, and/or special characters
 }
 
