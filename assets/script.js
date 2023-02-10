5d6656eefbf03a0edf786445cc287634c4f8cea5
smilillo <sofiemilillo@gmail.com>
2023-02-10 12:37:07 -0500

    added actual character options into arrays, seems like there must be a better way to do this so will come back to this later

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,10 @@
 
 // Variables Needed for Password Criteria
 let passwordLength = ;
-let lowerChar = ;
-let upperChar = ;
-let numChar = ;
-let specialChar = ;
+let lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let numChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", "=", "-", ".", "~", "|", "/", "?", "<", ">", "_", "{", "}", "[", "]"];
 
 //targets Generate Password Button
 var generateBtn = document.querySelector("#generate");
