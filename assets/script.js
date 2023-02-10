4fb2a65891c242dec8e68902285ac869e63404cf
smilillo <sofiemilillo@gmail.com>
2023-02-10 12:28:49 -0500

    added variables for dif characters possible for password

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,22 @@
 // Assignment Code
 
+// Variables Needed for Password Criteria
+let passwordLength = ;
+let lowerChar = ;
+let upperChar = ;
+let numChar = ;
+let specialChar = ;
+
 //targets Generate Password Button
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  
+  // prompts for password criteria
+  // 
+  // prompt for length between 8 and 128 characters
+  //  passwordLength = 
+  //   if (passwordLegth < 8 or > 128) return: "Please select a length between 8 and 128 characters"
+  // prompt for whether or not to include lowercase, uppercase, numeric, and/or special characters
 }
 
 // Write password to the #password input
@@ -22,12 +34,7 @@ function writePassword() {
 // Calls the writePassword function when the button is clicked
 generateBtn.addEventListener("click", writePassword);
 
-// prompts for password criteria
-// 
-// prompt for length between 8 and 128 characters
-//  passwordLength = 
-//   if (passwordLegth < 8 or > 128) return: "Please select a length between 8 and 128 characters"
-// prompt for whether or not to include lowercase, uppercase, numeric, and/or special characters
+
 // when prompts are completed run function to generate password
 // when password is generated display password in prompt or on screen
 
