6f1d9fabfb8d80dfdb26b5d77747cf2637a94149
smilillo <sofiemilillo@gmail.com>
2023-02-10 11:40:27 -0500

    added comments for plan to js file

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// prompts for password criteria
+// prompt for length between 8 and 128 characters
+// prompt for whether or not to include lowercase, uppercase, numeric, and/or special characters
+// when prompts are completed run function to generate password
+// when password is generated display password in prompt or on screen
+
